de01796dddd0c57bf564b486e2d4b9910c7e3dfc
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei.local>
2023-08-26 13:02:22 -0600

    probando subir cambios

--- clase2/desafio1.js
+++ clase2/desafio1.js
@@ -1,38 +1,47 @@
 //HANS ON LAB
 class ProductManager {
-   
-    constructor() {
-      this.products = [];
-    //   const product = {
-    //     title, description, price, thumbnail, code, stock
-    //   };
-    }
-   
-    addProduct(title, description, price, thumbnail, code, stock) {
-      const product = {
-        id: this.eventos.length
-          ? this.eventos[this.eventos.length - 1].code + 1  //como validar que no se repida el code?
-          : 1,
-          code,
-        title,
-        description,
-        price,
-        thumbnail, stock
-      };
-      this.products.push(product);
-    }
-  
-    getProducts(){
-        return [...products]
-    }
-    getProductsById(idProduct){
-        const product = this.products.find((e) => e.id === idProduct);
-        if(!product){
-            console.log('Not Found')
-        }
 
-    }
-   
-  
+  constructor() {
+    this.products = [];
   }
-  const algo= new ProductManager()
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      return 'Datos incompleto, ingresa todo los campos requeridos'
+    }
+
+    products.forEach(element => {
+      const allCodes = element.code
+      if (code === allCodes) {
+        console.log('este elemento ya existe')
+      }
+    })
+
+    const product = {
+      id: this.eventos.length
+        ? this.eventos[this.eventos.length - 1].code + 1
+        : 1,
+      code,
+      title,
+      description,
+      price,
+      thumbnail, stock
+    };
+    this.products.push(product);
+  }
+
+  getProducts() {
+    return [...products]
+  }
+  getProductsById(idProduct) {
+    const product = this.products.find((e) => e.id === idProduct);
+    if (!product) {
+      console.log('Not Found')
+    }
+
+  }
+
+
+}
+const algo = new ProductManager()
