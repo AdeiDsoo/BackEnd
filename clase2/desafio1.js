207c600d147cb60772b387e96fd81fbbc2239556
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei.local>
2023-08-26 14:14:14 -0600

    resolve desafio1

--- clase2/desafio1.js
+++ clase2/desafio1.js
@@ -1,24 +1,32 @@
 //HANS ON LAB
 class ProductManager {
-
   constructor() {
-   
     this.products = [];
   }
 
   addProduct(title, description, price, thumbnail, code, stock) {
+    // let codeExist;
 
     if (!title || !description || !price || !thumbnail || !code || !stock) {
-      return 'Datos incompleto, ingresa todo los campos requeridos'
+      return "Datos incompleto, ingresa todo los campos requeridos";
     }
-
-    this.products.forEach(element => {
-      const allCodes = element.code
-      if (code === allCodes) {
-        console.log('este elemento ya existe')
-      }
-    })
-
+    const productCode = this.products.find((e) => e.code === code);
+    if (productCode) {
+      return "ya existe este codigo";
+    }
+    
+    // this.products.forEach((element) => {
+    //   const allCodes = element.code;
+    //   if (code === allCodes) {
+    //     codeExist = true;
+    //   }
+    //   codeExist = false;
+    // });
+    // console.log(codeExist)
+    // if (codeExist === true) {
+   
+    //   return "este codigo ya existe";
+    // }
     const product = {
       id: this.products.length
         ? this.products[this.products.length - 1].id + 1
@@ -27,29 +35,49 @@ class ProductManager {
       title,
       description,
       price,
-      thumbnail, stock
+      thumbnail,
+      stock,
     };
     this.products.push(product);
   }
 
   getProducts() {
-    return [...products]
+    return [...this.products]
   }
   getProductsById(idProduct) {
     const product = this.products.find((e) => e.id === idProduct);
     if (!product) {
-      console.log('Not Found')
+      return "Not Found";
     }
-
+    console.log('si hay concidencia')
+    return product
   }
-
-
 }
-const product = new ProductManager()
+const product = new ProductManager();
 
-// const product1 = new ProductManager('title1', 'description1',' price1', 'thumbnail1', 'code1', 'stock1')
-// const product2 = new ProductManager('title2', 'description2',' price2', 'thumbnail2', 'code2', 'stock2')
+product.addProduct(
+  "title0",
+  "description0",
+  " price0",
+  "thumbnail0",
+  "code0",
+  "stock0"
+);
+product.addProduct(
+  "title1",
+  "description1",
+  " price1",
+  "thumbnail1",
+  "code1",
+  "stock1"
+);
+product.addProduct(
+  "title2",
+  "description2",
+  "price2",
+  "thumbnail2",
+  "code1",
+  "stock2"
+);
 
-product.addProduct('title0', 'description0',' price0', 'thumbnail0', 'code0', 'stock0')
-product.addProduct('title1', 'description1',' price1', 'thumbnail1', 'code1', 'stock1')
-console.log( product)
+console.log(product.getProducts());
