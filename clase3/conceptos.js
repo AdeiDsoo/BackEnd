e588e5831f92bdfa8f637ffc55919a932c24a124
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei.local>
2023-09-02 12:03:59 -0600

    notes first part class

---
+++ clase3/conceptos.js
@@ -0,0 +1,13 @@
+setTimeout(()=>{
+
+},5000)
+
+//setTimeout se ejecuta una vez
+//setInterval se ejecuta cada determinado tiempo indefinidamente
+//devuelve un apagador las paginas suelen tener un limite de tiempo para realizar determinada operacion ejemplo llenado de form
+
+setInterval(()=>{
+
+},interval)
+
+//clearInterval y existe  otra funcion para deternerlo ggoglearlo en mozilla
