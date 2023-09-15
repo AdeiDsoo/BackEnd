c181188fe0266f804e3f2bbe08145aed406cbadf
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei.local>
2023-09-14 20:59:18 -0600

    delivery of the challenge

--- clase4/index.js
+++
@@ -1,7 +0,0 @@
-// const http = require('http')    
-import http from 'http'
-
-const server= http.createServer()
-//crear el puerto
-server.listen(8080, ()=>console.log('Hola desdeee el puerto 8080'))
-//escucharlo
